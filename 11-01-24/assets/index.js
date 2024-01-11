64f8106cbba69520be7d2a144e8b51bd4973a176
DarkShadow77 <dadeshina4@gmail.com>
2024-01-11 11:12:28 -0800

    Class 2024

---
+++ 11-01-24/assets/index.js
@@ -0,0 +1,18 @@
+
+// function expression
+function nameOfFunction(){
+
+}
+
+//function invocation
+nameOfFunction()
+
+//function declaration
+
+let a = function(){}
+let b = () => {}
+
+//Immediately invoked function expression - IIFE
+(function greet(){
+    //expression
+})()
