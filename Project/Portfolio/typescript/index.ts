cb300be571d4fd152a5f8a3a4be8c0d18e0b0e71
DarkShadow77 <dadeshina4@gmail.com>
2024-03-01 17:25:09 -0800

    01-03-24

--- Project/Portfolio/typescript/index.ts
+++ Project/Portfolio/typescript/index.ts
@@ -2,7 +2,25 @@ let hover = document.getElementById("hover") as HTMLSpanElement;
 
 function onHover(index:number) {
     console.log(document.querySelector(`#nav :nth-child(${index})`));
-    if(index == 3){
-        hover.style.left = "700px"
+    if(index ==1){
+        hover.style.left = "630px"
     }
+    if(index == 2){
+        hover.style.left = "740px"
+    }
+    if(index == 3){
+        hover.style.left = "840px"
+    }
+    if(index == 4){
+        hover.style.left = "950px"
+    }
+    if(index == 5){
+        hover.style.left = "1080px"
+    }
+} 
+
+function onHoverOut() {
+    
+        hover.style.left = "630px"
+    
 } 
