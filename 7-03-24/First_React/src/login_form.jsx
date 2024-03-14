89ad9058adaabac1b480bd2cd87bcdc7f52b8261
DarkShadow77 <dadeshina4@gmail.com>
2024-03-14 09:43:17 -0800

    7-03-24

--- 7-03-24/First_React/src/login_form.jsx
+++ 7-03-24/First_React/src/login_form.jsx
@@ -4,7 +4,7 @@ function Login_Form() {
             <form action="">
                 <label htmlFor="email">Email:</label>
                 <input type="email" name="email" id="email" />
- <br />
+                <br />
                 <label htmlFor="password">Password:</label>
                 <input type="password" name="password" id="password" />
                 <button type="submit">Submit</button>
