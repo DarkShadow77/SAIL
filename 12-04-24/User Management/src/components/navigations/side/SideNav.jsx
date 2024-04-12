378ac10fea516b4827b8b2448738861b564dfae2
DarkShadow77 <dadeshina4@gmail.com>
2024-04-12 20:12:27 +0100

    12-04024

--- 12-04-24/User Management/src/components/navigations/side/SideNav.jsx
+++ 12-04-24/User Management/src/components/navigations/side/SideNav.jsx
@@ -1,9 +1,34 @@
+import Links from "../../links/Links";
+import { AiTwotoneHome, AiOutlineUser, AiFillSetting } from "react-icons/ai"
 const SideNav = () => {
-    return ( 
+
+    const path = [
+        {
+            to: "/home",
+            icon: <AiTwotoneHome />,
+            label: "Home"
+        },
+        {
+            to: "/users",
+            icon: <AiOutlineUser />,
+            label: "Users"
+        },
+        {
+            to: "/settings",
+            icon: <AiFillSetting />,
+            label: "Settings"
+        },
+    ]
+    
+    return (
         <>
-            <h3>Side Nav</h3>
+            {
+                path.map((value) => {
+                    return <Links to={value.to} icon={value.icon} label={value.label}></Links>
+                })
+            }
         </>
-     );
+    );
 }
- 
+
 export default SideNav;
--- 12-04-24/User Management/src/layout/MainLayout.jsx
+++ 12-04-24/User Management/src/layout/MainLayout.jsx
@@ -1,9 +1,14 @@
 import { Outlet } from "react-router-dom";
+import SideNav from "../components/navigations/side/SideNav";
+import style from "./mainlayout.module.css"
 
 const MainLayout = () => {
     return (
         <>
-            <Outlet/>
+            <div id={style.wrapper}>
+                <div id={style.sideNav}><SideNav /></div>
+                <div id={style.output}><Outlet /></div>
+            </div>
         </>
     );
 }
